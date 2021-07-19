358c726cd76aa91a022b3c3c6cbd038c87b0e026
Reachea <reacheasambath@gmail.com>
2021-07-19 07:36:59 +0700

    port changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 // app
 const app = express();
+const port = proces.env.PORT || 5000;
 
 // middlewares
 app.use(express.static('./frontend'));
@@ -10,8 +11,6 @@ app.use(express.static('./frontend'));
 
 
 // listening on port
-const port = proces.env.PORT || 5000
-
 app.listen(port, () => {
   console.log("listening on port:5000 🚀");
 });
